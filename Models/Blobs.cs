653e1ea6213773a354ef29d48fc03797810ac4a0
ElliotSolano <37352277+ElliotSolano@users.noreply.github.com>
2018-05-25 19:16:57 +0200

    Add files via upload

---
+++ Models/Blobs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TutorialBlobsApi.Models
+{
+    public class Blobs
+    {
+        public Blobs() { }
+
+        public Blobs(String nombre, String urlimg)
+        {
+            this.Nombre = nombre;
+            this.UrlImg = urlimg;
+        }
+
+        public String Nombre { get; set; }
+        public String UrlImg { get; set; }
+    }
+}
